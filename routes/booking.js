9fefd04a03fde9615baeb8132217eaf9728fb4df
Pahal Tiwari <seraphicspirit8650@gmail.com>
2026-05-02 16:21:43 +0530

    Update booking.js

--- routes/booking.js
+++ routes/booking.js
@@ -3,22 +3,6 @@ const router = express.Router();
 const bookingController = require("../controllers/booking.js");
 const { isLoggedIn } = require("../middleware");
 
-// router.get("/listings/:id/book", isLoggedIn, bookingController.renderForm);
-// router.post("/listings/:id/book", isLoggedIn, bookingController.createBooking);
-
-router.post("/bookings", async (req, res) => {
-    try {
-        const booking = new Booking(req.body);
-        await booking.save();
-
-        req.flash("success", "Booking Confirmed!");
-        return res.redirect("/listings"); // ✅ MUST
-
-    } catch (err) {
-        console.log(err);
-        req.flash("error", "Booking failed");
-        return res.redirect("back");
-    }
-});
-
+router.get("/listings/:id/book", isLoggedIn, bookingController.renderForm);
+router.post("/listings/:id/book", isLoggedIn, bookingController.createBooking);
 module.exports = router;
